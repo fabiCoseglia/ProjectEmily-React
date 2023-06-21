262b3fbc9d64ab3dc8d72052cbd1256a3bbbc48c
fabiCoseglia <fabiancoseglia@gmail.com>
2023-06-20 21:54:58 -0300

    fix Layout error to deploy

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
-import { Layout } from "./layout/layout";
+import { Layout } from "./layout/Layout";
 import { ChakraProvider } from "@chakra-ui/react";
 
 function App() {
